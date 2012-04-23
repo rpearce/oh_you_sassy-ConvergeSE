3588f9cedf12325af33eb5fcf415e60dea0f6df7
Robert Pearce <robertwaltonpearce@gmail.com>
2012-04-22 23:51:24 -0400

    Putting oh you sassy stuff as landing page

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
+PAGES = ['teachmehowtosassy', 'site1', 'site2', 'site3']
+
 get '/' do
   # I prefer HAML, but this'll do... -rwp
-  erb :site1
+  erb :index
+end
+
+PAGES.each do |page|
+  get "/#{page}" do
+    erb :"#{page}"
+  end
 end
